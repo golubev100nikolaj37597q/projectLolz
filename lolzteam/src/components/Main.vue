aa1d804ffb50593427175ae969b523ed37a3ccd2
danyablack <danyablack278@mail.ru>
2023-09-02 10:56:49 +0300

    Создал проект Vue

---
+++ lolzteam/src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+<!--Импоритируй header.scss-->
+
+
+<style>
+@import 'header.scss'
+</style>
